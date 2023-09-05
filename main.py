cd5dad15a8ac7dcd913ae24410adddfe7e6238a2
Andres J. Mayorga <httpmayorga@gmail.com>
2023-09-05 09:21:45 -0500

    Cdogio bien rearchar rama

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import readchar
+print("Inserte su nombre por favor")
 nombre=input()
 print("Bienvenido "+ nombre + " al laberinto de la anaconda")
 
 
 while True:
-    key = readchar.readkey()
-    print(f'Tecla presionada: {key}')
-    if key == '\x1b[A':  # Esto representa la tecla "UP"
-        break
-
-print('Programa terminado.')
+   key = readchar.readkey()
+   print(f"Tecla presionada: {key}")
+   if key == readchar.key.UP:
+         print("Se presionó la tecla de flecha hacia arriba. Saliendo del bucle.")
+         break
+    
