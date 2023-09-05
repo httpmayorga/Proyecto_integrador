d44e0195822a6066147aa9cdb023cfee972e26d5
Andres J. Mayorga <httpmayorga@gmail.com>
2023-09-05 09:27:06 -0500

    Vueleta al origen

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import readchar
+
 nombre=input()
 print("Bienvenido "+ nombre + " al laberinto de la anaconda")
 
 
-while True:
-    key = readchar.readkey()
-    print(f'Tecla presionada: {key}')
-    if key == '\x1b[A':  # Esto representa la tecla "UP"
-        break
 
-print('Programa terminado.')
+
